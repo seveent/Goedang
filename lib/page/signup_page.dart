8214924f51ecf0600ef49a7068809491288765e5
seveent <144870441+seveent@users.noreply.github.com>
2024-02-28 07:20:04 +0700

    signup_page.dart

--- lib/page/signup_page.dart
+++ lib/page/signup_page.dart
@@ -1,168 +1,200 @@
 import 'package:flutter/material.dart';
 import 'package:login_flutter/page/login_page.dart';
 
-
-
-
 class SignUpPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => StartState();
 }
 
-class StartState extends State<SignUpPage>{
+class StartState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return initWidget();
   }
 
-
-initWidget(){
-
-  return Scaffold(
-    body: SingleChildScrollView(
-      child: Column(
-        children: [
-          Container(
-              height: 300,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(90), bottomRight: Radius.circular(90)
+  initWidget() {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+                height: 300,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(90),
+                      bottomRight: Radius.circular(90)),
+                  color: Color.fromARGB(255, 247, 161, 1),
                 ),
-                color: Color.fromARGB(255, 247, 161, 1),
-              ),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(top: 50),
-                      child: Image.asset("img/goedang_logo.png"),
-                      height: 90,
-                      width: 90,
-                    )
-                  ],
-                ),
-              )
-          ),
-
-          Container(
-            alignment: Alignment.center,
-            margin: EdgeInsets.only(left: 20, right: 20, top: 20),
-            padding: EdgeInsets.only(left: 20, right: 20),
-            height: 54,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                color: Colors.grey[300],
-                boxShadow:[
-                  BoxShadow(
-                      offset: Offset(0, 10),
-                      blurRadius: 50,
-                      color: Color(0xffEEEEEE)
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 50),
+                        child: Image.asset("img/goedang_logo.png"),
+                        height: 90,
+                        width: 90,
+                      )
+                    ],
                   ),
-                ]
-            ),
-            child: TextField(
-              cursorColor: Color(0xff1393aa),
-              decoration: InputDecoration(
-                  icon: Icon(
-                    Icons.email,
-                    color: Color.fromARGB(255, 247, 161, 1),
-                  ),
-                  hintText: "Email",
-                  enabledBorder: InputBorder.none,
-                  focusedBorder: InputBorder.none
-              ),
-            ),
-          ),
-
-          Container(
-            alignment: Alignment.center,
-            margin: EdgeInsets.only(left: 20, right: 20, top: 20),
-            padding: EdgeInsets.only(left: 20, right: 20),
-            height: 54,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                color: Colors.grey[300],
-                boxShadow:[
-                  BoxShadow(
-                      offset: Offset(0, 10),
-                      blurRadius: 50,
-                      color: Color(0xffEEEEEE)
-                  ),
-                ]
-            ),
-            child: TextField(
-              cursorColor: Color(0xff1393aa),
-              decoration: InputDecoration(
-                  icon: Icon(
-                    Icons.vpn_key,
-                    color: Color.fromARGB(255, 247, 161, 1),
-                  ),
-                  hintText: "Password",
-                  enabledBorder: InputBorder.none,
-                  focusedBorder: InputBorder.none
-              ),
-            ),
-          ),
-
-          GestureDetector(
-            onTap: (){
-            },
-            child: Container(
+                )),
+            Container(
               alignment: Alignment.center,
               margin: EdgeInsets.only(left: 20, right: 20, top: 20),
               padding: EdgeInsets.only(left: 20, right: 20),
               height: 54,
               decoration: BoxDecoration(
-                gradient: LinearGradient(colors: [(Color.fromARGB(255, 247, 161, 1)),(Color.fromARGB(255, 247, 161, 1))],
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.grey[300],
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: Color(0xffEEEEEE)),
+                  ]),
+              child: TextField(
+                cursorColor: Color(0xff1393aa),
+                decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.email,
+                      color: Color.fromARGB(255, 247, 161, 1),
+                    ),
+                    hintText: "Email Address",
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              height: 54,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.grey[300],
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: Color(0xffEEEEEE)),
+                  ]),
+              child: TextField(
+                cursorColor: Color(0xff1393aa),
+                decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.account_box,
+                      color: Color.fromARGB(255, 247, 161, 1),
+                    ),
+                    hintText: "Nama",
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              height: 54,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.grey[300],
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: Color(0xffEEEEEE)),
+                  ]),
+              child: TextField(
+                cursorColor: Color(0xff1393aa),
+                decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.vpn_key,
+                      color: Color.fromARGB(255, 247, 161, 1),
+                    ),
+                    hintText: "Set Password",
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              height: 54,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.grey[300],
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: Color(0xffEEEEEE)),
+                  ]),
+              child: TextField(
+                cursorColor: Color(0xff1393aa),
+                decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.vpn_key,
+                      color: Color.fromARGB(255, 247, 161, 1),
+                    ),
+                    hintText: "Cofirm Password",
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                padding: EdgeInsets.only(left: 20, right: 20),
+                height: 54,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                    (Color.fromARGB(255, 247, 161, 1)),
+                    (Color.fromARGB(255, 247, 161, 1))
+                  ], begin: Alignment.centerLeft, end: Alignment.centerRight),
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.grey[300],
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: Color(0xffEEEEEE))
+                  ],
                 ),
-                borderRadius: BorderRadius.circular(50),
-                color: Colors.grey[300],
-                boxShadow: [BoxShadow(
-                    offset: Offset(0, 10),
-                    blurRadius: 50,
-                    color: Color(0xffEEEEEE)
-                )
+                child: Text(
+                  "SIGN UP",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Sudah punya akun?"),
+                  GestureDetector(
+                    child: Text(
+                      "Login",
+                      style: TextStyle(color: Color.fromARGB(255, 247, 161, 1)),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginPage(),
+                          ));
+                    },
+                  )
                 ],
               ),
-              child: Text(
-                "SIGN UP",
-                style: TextStyle(
-                    color: Colors.white
-                ),
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Sudah punya akun?"),
-                GestureDetector(
-                  child: Text(
-                    "Login",
-                    style: TextStyle(
-                        color: Color.fromARGB(255, 247, 161, 1)
-                    ),
-                  ),
-                  onTap: (){
-                    Navigator.push(context,
-                        MaterialPageRoute(
-                            builder: (context) => LoginPage(),
-                        )
-                    );
-                  },
-                )
-              ],
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 }
